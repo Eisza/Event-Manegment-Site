03d4d7abb3dcc1d6c12f02a3093aa251c52f2e59
Eisza <50252639+Eisza@users.noreply.github.com>
2019-12-23 21:12:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var users = [
     {
         name:"YasserRahmo",
@@ -25,7 +26,7 @@ var users = [
         admin:true
     }
 ];
-
+var user;
 var isLoggedIn = false;
 var isAdmin = false;
 
@@ -43,12 +44,13 @@ if(window.location.href.indexOf("username=")){
             if(users[i].pass === password){
                 isLoggedIn = true;
                 isAdmin = users[i].admin;
+                user=users[i];
                 
             }
         }
     }
 
-    console.log(isAdmin);
+    console.log(user);
 }
 function handleLogin(){
     var username = document.getElementById("username").value;
@@ -65,5 +67,29 @@ function handleLogin(){
     if(!isLoggedIn){
         alert("username or password incorrect");
         return false;
+    }
+}
+
+function handleLogout(){
+    window.location.href = "index.html";
+}
+
+function loadAboutUs(){
+    document.getElementById("container").innerHTML =`<div  class="paragraph" id="paragraph">
+    <p>
+        <h1>About Us</h1>
+        <br>
+        Unit 22 is an Egyptian company that was established in 2007 under the name “Masterpiece” that later in 2011 evolved to “The Hub Production” and starting 2016 “Unit 22” was brought to life! “Unit 22” is shielded with years of experience, in both local and regional events. Our professional team members are sharp event hunters with spines of talent and creativity, dedicated to make your brand stand out through exceptional event management services. Our fresh approach in event execution is tailored to suit your strategic vision and defined milestones. Aided by our network of preferred vendors, the industry’s top suppliers and connections to local and international entertainment providers, rest assured that your event will be the talk of town.
+    </p>
+
+
+</div>`
+}
+
+//index changes
+window.onload = function(){
+    if(user){
+        document.getElementById("loginLink").setAttribute('onclick','handleLogout()');
+        document.getElementById("loginLink").innerHTML = "<li>Logout</li>";
     }
 }
