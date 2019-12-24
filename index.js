62afdce9316e1d8a22c1e37b0bfddbceb5d154f2
eisa <g45@outlook.com>
2019-12-24 05:58:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -169,18 +169,114 @@ var events = [
     {
         name:"first",
         _id:"001",
-        date:new Date(),
+        date:"9/jan",
+        description:"Middle East Music Event is showcasing the very best of the region's alternative talent and cutting-edge emerging artists, all on one line up for the first time. Non-stop music over two stages and three days.",
+        status:true,
+        participants:["abdo","bgdo"]
+    },
+    {
+        name:"second",
+        _id:"002",
+        date:"12/jan",
+        description:"Middle East Music Event is showcasing the very best of the region's alternative talent and cutting-edge emerging artists, all on one line up for the first time. Non-stop music over two stages and three days.",
         status:true,
         participants:["abdo","bgdo"]
     }
 ];
+idCounter = 3;
+
 
 function loadEvents(){
+    var eventsString="" ;
 
-}
+    for(var i = 0; i < events.length ; i++){
+        eventsString+=`
+        <li>
+            <div class="time" id="time">
+                <h2>${events[i].date.split("/")[0]}<br><span>${events[i].date.split("/")[1]}</span><br><span>${events[i].status?`active`:`canceled`}</span></h2>
+            </div>
+            <div class="details" id="details">
+                <h3>${events[i].name}</h3>
+                <p>${events[i].description}</p>
+                <a style="float: right;">Ps: ${events[i].participants.length}</a>
+                ${user?`<a onclick="participate('${events[i]._id}')">sign in</a>`:``}
+            </div>
+        </li>
+        <br>`;
+    };
+    document.getElementById("container").innerHTML ="<div class='events' id='events'><div class='content' id='content'><ul>"+
+            eventsString+
+            "</ul></div></div>";
 
-function addParticipant(event){
-    for(var i = 0 ; i < events.length ; i++){
+    if(isAdmin){
+        document.getElementById("content").innerHTML +=`
+        <div>
+            <label for="eventName">event name:</label>
+            <input id="eventName" type="text">
+            <br>
+            <label for="eventDate">event date:</label>
+            <input id="eventDate" type="text">
+            <br>
+            <label for="eventDesc">event desc.:</label>
+            <input id="eventDesc" type="text">
+            <br>
+            <button onclick="addEvent()">add event</button>
+        </div>
         
+        `;
+        for(var i = 0 ; i < events.length ; i++){
+            var list=``;
+            for(var j = 0 ; j < events[i].participants.length ; j++){
+                list+= `<li>${events[i].participants[j]}</li><br>`
+                
+            }
+            document.getElementById("events").innerHTML +=`
+                <div class="lop">
+                    <h5>participants of ${events[i].name}</h5>
+                    <ol>
+                    ${list}
+                    </ol>
+                </div>`
+        }
     }
 }
+
+function participate(id){
+    var found = false;
+    for(var i = 0 ; i < events.length ; i++){
+        if(events[i]._id == id){
+            for(var j = 0 ; j < events[i].participants.length ; j++){
+                if(events[i].participants[j]===user.name){
+                    alert("already signed in");
+                    found = true;
+                }
+            }
+            if(!found){
+                events[i].participants.push(user.name);
+                console.log(events[i].participants)
+            }
+        }
+    }
+    loadEvents();
+    
+}
+function addEvent(){
+    if(document.getElementById("eventDate").value.indexOf("/") < 1){
+        alert("date format must be d/m");
+    }
+    else if(!document.getElementById("eventName").value === ""){
+        alert("must put name");
+    }
+    var newEvent = {
+        name:document.getElementById("eventName").value,
+        _id:`00${idCounter++}`,
+        date: document.getElementById("eventDate").value,
+        description:document.getElementById("eventDesc").value,
+        status:true,
+        participants:[]
+    }
+    events.push(newEvent);
+    loadEvents();
+}
+
+
